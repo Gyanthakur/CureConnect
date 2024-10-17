3c6280fc59911961dc9d9e83b7dc24c9794d37d0
Gyanthakur <gps.96169@gmail.com>
2024-10-17 16:02:09 +0530

    feat: added pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import AppContextProvider from "./context/AppContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+	<BrowserRouter>
+		<AppContextProvider>
+			<App />
+		</AppContextProvider>
+	</BrowserRouter>
+);
